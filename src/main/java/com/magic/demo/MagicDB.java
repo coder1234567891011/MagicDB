add750cea70723e931618b85e7ca0fc9353d6a58
coder1234567891011 <wesyoung98@gmail.com>
2024-07-05 08:34:35 -0400

    Today's commit

--- src/main/java/com/magic/demo/MagicDB.java
+++ src/main/java/com/magic/demo/MagicDB.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MagicDB {
-
+	//commit for my babies bday
 	public static void main(String[] args) {
 		SpringApplication.run(MagicDB.class, args);
 	}
